6445ffae4dc4ddbe3c97bb1e697efde8f36fae28
Arian-Lame <arianlame@hotmail.com>
2025-09-26 14:01:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Dockerized Flask app!"
+    return "Hello from Dockerized Flask app and Hello from CI/CD v2 🚀"
+!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
